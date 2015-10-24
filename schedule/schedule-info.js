46e56537e23654b16a53a89f66a61d74707ca614
Barry Grant <bjgrant@umich.edu>
2015-10-23 22:50:46 -0400

    First template setup with morea

---
+++ schedule/schedule-info.js
@@ -0,0 +1,4 @@
+moreaEventData = [
+  {title: "Experience the Baz", url: "/bioinf525_w16/morea/baz/experience.html", start: "2015-06-11T18:00"},
+  {title: "Learn to Baz", url: "/bioinf525_w16/modules/baz", start: "2015-06-11", end: "2015-06-15"}
+]
